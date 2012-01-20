53fa58c8f48391dde5debb7448fc2c4d6fb6dd5b
Rutger Vos <rutgeraldo@gmail.com>
2012-01-20 16:50:20 -0500

    Adding MapReduce mapper

---
+++ map.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my ( $key, $infile ) = @ARGV;
+my @fields;
+{
+	open my $fh, '<', $infile or die $!;
+	my @lines = <$fh>;
+	close $fh;
+	@fields = split /\t/, $lines[0];
+}
+if ( $key eq $fields[0] ) {
+	for my $i ( 1 .. $#fields ) {
+		print "UniqValueCount:", $fields[$i-1], "\t", $fields[$i], "\n";
+	}
+}
