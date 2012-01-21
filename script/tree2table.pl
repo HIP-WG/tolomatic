5e33f3b5285857f1522a5c135420f773f21f6328
Rutger Vos <rutgeraldo@gmail.com>
2012-01-21 09:06:10 -0500

    Refactored to make file names the MD5 hex hash of the terminal node name.

---
+++ script/tree2table.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+use Bio::Phylo::IO 'parse';
+use Bio::Phylo::Util::CONSTANT ':objecttypes';
+use Digest::MD5 'md5_hex';
+use Getopt::Long;
+
+my ( $file, $format, $dir, $url );
+GetOptions(
+	'file=s'   => \$file,
+	'url=s'    => \$url,
+	'format=s' => \$format,
+	'dir=s'    => \$dir,
+);
+
+mkdir $dir if not -d $dir;
+my %args = ( 
+	'-format'     => $format,
+	'-as_project' => 1,
+);
+$file ? $args{'-file'} = $file : $url ? $args{'-url'} = $url : die "Need -file or -url";
+my ($tree) = @{ parse(%args)->get_items(_TREE_) };
+
+$tree->visit_depth_first(
+	'-pre' => sub {
+		my $node = shift;
+		if ( my $parent = $node->get_parent ) {
+			my @path = @{ $parent->get_generic('path') };
+			unshift @path, $node->get_internal_name;
+			$node->set_generic( 'path' => \@path );
+			if ( $node->is_terminal ) {
+				my $filename = md5_hex($path[0]);
+				open my $fh, '>', "${dir}/${filename}" or die "Can't open ${dir}/${filename}: $!";
+				print $fh join "\t", @path;
+				close $fh;
+			}
+		}
+		else {
+			$node->set_generic( 'path' => [ $node->get_internal_name ] );
+		}
+	}
+);
