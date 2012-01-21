ddf51749edd0b0def5248481401050d9e6585b60
Rutger Vos <rutgeraldo@gmail.com>
2012-01-21 09:00:36 -0500

    Deleted old script/prune.sh

--- script/prune.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-$HADOOP_HOME/bin/hadoop  jar $HADOOP_HOME/hadoop-$HADOOP_VERSION-streaming.jar \
-    -input `pwd`/supertree \
-    -output `pwd`/tolout \
-    -mapper map.pl \
-    -file map.pl \
-    -file taxa.txt \
-    -reducer aggregate \
-    -verbose
