eb7a1c6da02a54e749778683d772813b128e094d
Rutger Vos <rutgeraldo@gmail.com>
2012-01-22 08:07:22 -0500

    This now basically works, except that it assumes that parent nodes are numbered in post-order traversal such that the removal of unbranched internals can be completed by incrementing orphaned internal node indices to the nearest higher value.

--- lib/Bio/PhyloTastic/Pruner/MapReduce.pm
+++ lib/Bio/PhyloTastic/Pruner/MapReduce.pm
@@ -1,26 +1,82 @@
 package Bio::PhyloTastic::Pruner::MapReduce;
 use Moose::Role;
+use Scalar::Util 'refaddr';
+
+die "Need DATADIR environment variable" if not -d $ENV{'DATADIR'};
 
 sub map { 
-    my ($self, $line) = @_;
-    die "FIXME";
-    my ( $key, $value );
-    $self->emit( $key => $value);
+    my ($self, $taxon) = @_;
+    my $file = $ENV{'DATADIR'} . '/' . $taxon;
+    open my $fh, '<', $file or die "Can't open ${file}: $!";
+    my @lines = <$fh>;
+    my @fields = split /\t/, $lines[0];    
+    for my $i ( 1 .. $#fields ) {
+        $self->emit( 'child:'.$fields[$i-1], 'parent:'.$fields[$i] );        
+    }
 }
 
-sub reduce { 
-    my ( $self, $key, $value_iterator) = @_;
-    die "FIXME";
-    while( $value_iterator->has_next() ) { }
-    my $composite_value;
-    $self->emit( $key, $composite_value );
-} 
+{
+    my %seen_branch;
+    my %child_of;
+    sub combine { 
+        my ( $self, $child, $iter ) = @_;
+        ITEM: while( $iter->has_next ) {
+            my $parent = $iter->next;
+            my $branch = "$child,$parent";
+            
+            # this so that we don't print out duplicate branches, which
+            # occur for branches that are on the path to the root for
+            # multiple nodes
+            $seen_branch{$branch}++;
+            next ITEM if $seen_branch{$branch} > 1;
+            
+            # this so that we only print parents the second time they are
+            # seen, i.e. to prevent unbranched internals and unbranched
+            # paths below the root
+            if ( not exists $child_of{$parent} ) {
+                $child_of{$parent} = $child;
+                if ( $child =~ /^child:\d+$/ ) {
+                    next ITEM;
+                }
+            }
+            else {
+                $self->emit( $child_of{$parent}, $parent ) if defined $child_of{$parent};
+                $child_of{$parent} = undef;
+            }
+            $self->emit( $child, $parent );
+        }
+    }
+}
 
-sub combine { 
-    my ( $self, $key, $value_iterator) = @_;
-    while( $value_iterator->has_next() ) { die "FIXME" }
-    my $composite_value;
-    $self->emit( $key, $composite_value );
+{
+    my %parent_of;
+    my %seen_parent;
+    sub reduce { 
+        my ( $self, $child, $iter ) = @_;
+        while ( $iter->has_next ) {
+            my $parent = $iter->next;
+            
+            if ( $child !~ /^child:\d+$/ ) {
+                $self->emit( $child, $parent );
+            }
+            
+            # if the branch is an internal branch, i.e. $key =~ /^child:/, only
+            # print it out if we've seen the child more than once as a parent            
+            $seen_parent{$parent}++;
+            $parent_of{$child} = $parent;
+            for my $stored_parent ( keys %seen_parent ) {
+                if ( $seen_parent{$stored_parent} == 2 ) {
+                    my $parent_as_child = $stored_parent;
+                    $parent_as_child =~ s/^parent:/child:/;
+                    if ( $parent_of{$parent_as_child} ) {
+                        $self->emit( $parent_as_child, $parent_of{$parent_as_child} );
+                        delete $parent_of{$parent_as_child};
+                        delete $seen_parent{$stored_parent};
+                    }
+                }
+            }
+        }
+    }
 }
 
 package Bio::PhyloTastic::Pruner::Mapper;
