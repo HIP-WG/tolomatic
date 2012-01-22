1612221b5ca3935d150388265ec44349ecf54258
Rutger Vos <rutgeraldo@gmail.com>
2012-01-22 20:41:17 +0100

    Changed command line parameters for ToLWeb test

--- script/pruner/prune.sh
+++ script/pruner/prune.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 TOLOMATIC=/Users/rvosa/Dropbox/documents/projects/current/tolomatic/
-rm -rf $TOLOMATIC/examples/primates/result
+rm -rf $TOLOMATIC/examples/tolweb/result
 $HADOOP_HOME/bin/hadoop  jar $HADOOP_HOME/hadoop-$HADOOP_VERSION-streaming.jar \
-    -cmdenv DATADIR=$TOLOMATIC/examples/primates/nodes \
-    -input $TOLOMATIC/examples/primates/taxa.txt \
-    -output $TOLOMATIC/examples/primates/result \
+    -cmdenv DATADIR=$HOME/Desktop/tolomatic \
+    -input $TOLOMATIC/examples/tolweb/taxa.txt \
+    -output $TOLOMATIC/examples/tolweb/result \
     -mapper mapper.pl \
     -combiner combiner.pl \
     -reducer reducer.pl \
