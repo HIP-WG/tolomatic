4e791977bfa7510e032a309feb33e801c4e445db
Rutger Vos <rutgeraldo@gmail.com>
2012-01-22 08:05:14 -0500

    Changed mode to u=rwx

--- script/pruner/prune.sh
+++ script/pruner/prune.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+TOLOMATIC=/Users/rvosa/Dropbox/documents/projects/current/tolomatic/
+rm -rf $TOLOMATIC/examples/primates/result
 $HADOOP_HOME/bin/hadoop  jar $HADOOP_HOME/hadoop-$HADOOP_VERSION-streaming.jar \
-    -input `pwd`/supertree \
-    -output `pwd`/tolout \
-    -mapper map.pl \
-    -file map.pl \
-    -file taxa.txt \
-    -reducer aggregate \
+    -cmdenv DATADIR=$TOLOMATIC/examples/primates/nodes \
+    -input $TOLOMATIC/examples/primates/taxa.txt \
+    -output $TOLOMATIC/examples/primates/result \
+    -mapper mapper.pl \
+    -combiner combiner.pl \
+    -reducer reducer.pl \
     -verbose
