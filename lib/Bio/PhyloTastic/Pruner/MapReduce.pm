eab0f90649ea82b4f889b635dbd2e0a67e852ca3
Rutger Vos <rutgeraldo@gmail.com>
2012-01-23 00:28:52 +0100

    Updated to a much better algorithm, see POD.

--- lib/Bio/PhyloTastic/Pruner/MapReduce.pm
+++ lib/Bio/PhyloTastic/Pruner/MapReduce.pm
@@ -1,82 +1,94 @@
 package Bio::PhyloTastic::Pruner::MapReduce;
 use Moose::Role;
 use Scalar::Util 'refaddr';
+use Bio::Phylo::Util::Logger ':levels';
 use Digest::MD5 'md5_hex';
+use Data::Dumper;
 
 die "Need DATADIR environment variable" if not -d $ENV{'DATADIR'};
 
+my $logger = Bio::Phylo::Util::Logger->new( '-level' => INFO );
+
+=item map($self,$taxon)
+
+Given a single taxon name as argument, this method reads in a file inside
+$ENV{DATADIR} whose name is the MD5 hex code hash of the taxon name. That
+file should contain one line: a tab-separate list that describes the path,
+in post-order indexed integers, from taxon to the root. Each segment of
+that path is emitted as node ID => taxon. For example, for tree
+(((A,B)1,C)2,D)3; if taxon is A, this emits:
+
+ 1,A
+ 2,A
+ 3,A
+
+=cut
+
 sub map { 
-    my ($self, $taxon) = @_;
+    my ( $self, $taxon ) = @_;
     my $file = $ENV{'DATADIR'} . '/' . md5_hex($taxon);
+    $logger->info("taxon: $taxon (file: $file)");
     open my $fh, '<', $file or die "Can't process taxon ${taxon} (${file}): $!";
     my @lines = <$fh>;
-    my @fields = split /\t/, $lines[0];    
+    my @fields = split /\t/, $lines[0];
+    $logger->debug("path: @fields");
     for my $i ( 1 .. $#fields ) {
-        $self->emit( 'child:'.$fields[$i-1], 'parent:'.$fields[$i] );        
+        $self->emit( $fields[$i], $fields[0] );        
     }
 }
 
-{
-    my %seen_branch;
-    my %child_of;
-    sub combine { 
-        my ( $self, $child, $iter ) = @_;
-        ITEM: while( $iter->has_next ) {
-            my $parent = $iter->next;
-            my $branch = "$child,$parent";
-            
-            # this so that we don't print out duplicate branches, which
-            # occur for branches that are on the path to the root for
-            # multiple nodes
-            $seen_branch{$branch}++;
-            next ITEM if $seen_branch{$branch} > 1;
-            
-            # this so that we only print parents the second time they are
-            # seen, i.e. to prevent unbranched internals and unbranched
-            # paths below the root
-            if ( not exists $child_of{$parent} ) {
-                $child_of{$parent} = $child;
-                if ( $child =~ /^child:\d+$/ ) {
-                    next ITEM;
-                }
-            }
-            else {
-                $self->emit( $child_of{$parent}, $parent ) if defined $child_of{$parent};
-                $child_of{$parent} = undef;
-            }
-            $self->emit( $child, $parent );
-        }
+=item combine($self,$node,$iter)
+
+Given a node ID (as described for C<map>) as a key, and all the tips that have
+that node on their respective paths to the root, this method combines these to
+emit a concatenated list of all tips, then the node ID, then the tip count. E.g.
+if the node ID is 1, this will be passed:
+ 
+ 1,A,B
+ 
+and will emit:
+
+ A|B,1,2
+
+=cut
+
+sub combine { 
+    my ( $self, $node, $iter ) = @_;    
+    my @tips;
+    while( $iter->has_next ) {
+        push @tips, $iter->next;        
+    }
+    $logger->debug("node: $node tips: @tips");
+    if ( @tips ) {
+        $self->emit( join("|", sort{$a cmp $b} @tips), $node . ',' . scalar(@tips) );
     }
 }
 
-{
-    my %parent_of;
-    my %seen_parent;
-    sub reduce { 
-        my ( $self, $child, $iter ) = @_;
-        while ( $iter->has_next ) {
-            my $parent = $iter->next;
-            
-            if ( $child !~ /^child:\d+$/ ) {
-                $self->emit( $child, $parent );
-            }
-            
-            # if the branch is an internal branch, i.e. $key =~ /^child:/, only
-            # print it out if we've seen the child more than once as a parent            
-            $seen_parent{$parent}++;
-            $parent_of{$child} = $parent;
-            for my $stored_parent ( keys %seen_parent ) {
-                if ( $seen_parent{$stored_parent} == 2 ) {
-                    my $parent_as_child = $stored_parent;
-                    $parent_as_child =~ s/^parent:/child:/;
-                    if ( $parent_of{$parent_as_child} ) {
-                        $self->emit( $parent_as_child, $parent_of{$parent_as_child} );
-                        delete $parent_of{$parent_as_child};
-                        delete $seen_parent{$stored_parent};
-                    }
-                }
-            }
-        }
+=item reduce($self,$tips,$iter)
+
+Given a concatenated list of tips, a node ID and the number of tips it subtends,
+this will filter out all nodes that subtend 1 tip. In cases where a concatenated
+list of tips has multiple values associated with it, this means that there are
+unbranched internal nodes on the path from those tips to the root. Of those
+unbranched internals we want the MRCA of the tips, which we obtain by sorting
+the node IDs: since these are applied in post-order, the lowest node ID in the
+list is the MRCA.
+
+=cut
+
+sub reduce { 
+    my ( $self, $tips, $iter ) = @_;
+    my @nodes;
+    while ( $iter->has_next ) {
+        my %tuple;
+        ( $tuple{node}, $tuple{count} ) = split /,/, $iter->next;
+        push @nodes, \%tuple;
+    }
+    my @sorted = sort { $a->{node} <=> $b->{node} } grep { $_->{count} > 1 } @nodes;
+    if ( @sorted ) {
+        my $mrca = $sorted[0];
+        $logger->info("mrca: " . $mrca->{node} . " tips: " . $tips);
+        $self->emit( $tips, $mrca->{node} . ',' . $mrca->{count} );
     }
 }
 
@@ -93,3 +105,4 @@ use Moose;
 with 'Hadoop::Streaming::Reducer', 'Bio::PhyloTastic::Pruner::MapReduce';
 
 1;
+
