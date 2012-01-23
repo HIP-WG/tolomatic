05d752a89010418474e2869c45592aa1b8f42be8
Rutger Vos <rutgeraldo@gmail.com>
2012-01-23 23:18:39 +0100

    Updated to use bigger species lists

--- script/pruner/prune.sh
+++ script/pruner/prune.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 TOLOMATIC=/Users/rvosa/Dropbox/documents/projects/current/tolomatic/
-rm -rf $TOLOMATIC/examples/tolweb/result
+RESULT=$TOLOMATIC/examples/tolweb/result
+rm -rf $RESULT
 $HADOOP_HOME/bin/hadoop  jar $HADOOP_HOME/hadoop-$HADOOP_VERSION-streaming.jar \
     -cmdenv DATADIR=$HOME/Desktop/tolomatic \
-    -input $TOLOMATIC/examples/tolweb/taxa.txt \
-    -output $TOLOMATIC/examples/tolweb/result \
+    -input $TOLOMATIC/examples/tolweb/mammals.txt \
+    -output $RESULT \
     -mapper mapper.pl \
     -combiner combiner.pl \
     -reducer reducer.pl \
