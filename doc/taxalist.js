578774693b128fe73aa7f329b966fcc2e4e3f6e7
Rutger Vos <rutgeraldo@gmail.com>
2013-01-30 12:54:25 -0700

    adding example "clean" list

---
+++ doc/taxalist.js
@@ -0,0 +1,27 @@
+[
+	{
+		"name" : "Methanocaldococcus fervens str. AG86",
+		"id" : 16,
+		"uri" : "http://www.ncbi.nlm.nih.gov/taxonomy/573064"
+	},
+	{
+		"name" : "Methanocaldococcus vulcanius str. M7",
+		"id" : 17,
+		"uri" : "http://www.ncbi.nlm.nih.gov/taxonomy/579137"
+	},
+	{
+		"name" : "Methanococcus infernus",
+		"id" : 20,
+		"uri" : "http://www.ncbi.nlm.nih.gov/taxonomy/67760"
+	},
+	{
+		"name" : "Methanococcus vannielii",
+		"id" : 37,
+		"uri" : "http://www.ncbi.nlm.nih.gov/taxonomy/2187"
+	},
+	{
+		"name" : "Methanococcus maripaludis str. LL",
+		"id" : 43,
+		"uri" : "http://www.ncbi.nlm.nih.gov/taxonomy/39152"
+	}
+]
