db410f22adc429f67cb217ab50f06b65afb1ca09
Rutger Vos <rutgeraldo@gmail.com>
2013-01-31 09:46:15 -0700

    this is still not working the way I want it inside Apache2/mod_perl. the hadoop jar is not being unpacked correctly.

--- lib/Bio/PhyloTastic/Pruner/Runner.pm
+++ lib/Bio/PhyloTastic/Pruner/Runner.pm
@@ -1,45 +1,51 @@
-package Bio::PhyloTastic::MapReduce::Runner;
+package Bio::PhyloTastic::Pruner::Runner;
 use strict;
 use warnings;
 use File::Path qw(remove_tree);
 use Bio::PhyloTastic::Pruner::Util;
 use File::Temp qw(tempfile tempdir);
-use Bio::PhyloTastic::Pruner::CONSTANT qw(TREEID TAXALIST);
+use Bio::PhyloTastic::Pruner::CONSTANT qw(TREEID TAXALIST TAXONID);
 
 # fetch the constants
 my $treeid   = TREEID;
 my $taxalist = TAXALIST;
+my $taxonid  = TAXONID;
 
 # instantiate helper objects
 my $util = Bio::PhyloTastic::Pruner::Util->new;
+my $log  = Bio::Phylo::Util::Logger->new( '-level' => $util->{_}->{loglevel} );
 
 sub run {
 	my $self = shift;
 	my %args = @_;
 	
 	# the input file is a list of identifiers, one per line
-	my @taxa = @_;
 	my ( $fh, $infile ) = tempfile();
-	print $fh join "\n", @{ $args{$taxalist} };
-	close $fh;
+	print $fh join "\n", map { $_->{$taxonid} } @{ $args{$taxalist} };
+	close $fh;	
 	
 	# hadoop wants the output dir to be empty - nay, not even there!
 	my $tmpdir = $util->{_}->{tmpdir};
-	remove_tree($tmpdir);
-
-	# invoke hadoop
-	system(
+	$log->info("infile is $infile");
+	$log->info("tmpdir is $tmpdir");
+	
+	# compose command
+	my @command = (
 		"$ENV{HADOOP_HOME}/bin/hadoop",
 		'jar'       => "$ENV{HADOOP_HOME}/hadoop-$ENV{HADOOP_VERSION}-streaming.jar",
 		'-cmdenv'   => 'PHYLOTASTIC_MAPREDUCE_TREE='   . $args{$treeid},
 		'-cmdenv'   => 'PHYLOTASTIC_MAPREDUCE_CONFIG=' . $ENV{PHYLOTASTIC_MAPREDUCE_CONFIG},
-		'-cmdenv'   => 'PERL5LIB=' . join ':', @INC,
+		'-cmdenv'   => 'PERL5LIB=' . join( ':', @INC ),
 		'-input'    => $infile,
 		'-output'   => $tmpdir,
 		'-mapper'   => $util->{_}->{mapper},
 		'-combiner' => $util->{_}->{combiner},
 		'-reducer'  => $util->{_}->{reducer},
-	) == 0 or die $?;
+	);
+	$log->info(join ' ',@command);
+
+	# invoke hadoop
+	system( @command ) == 0 or die $?;
 	
 	# return value is the path to the outfile
 	return "$tmpdir/part-00000";
