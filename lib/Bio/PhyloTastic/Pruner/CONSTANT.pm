665e3c0067732c8ef5bb7d36bec2f27796831c33
Rutger Vos <rutgeraldo@gmail.com>
2013-01-31 09:35:14 -0700

    added constant for treestoreId predicate

--- lib/Bio/PhyloTastic/Pruner/CONSTANT.pm
+++ lib/Bio/PhyloTastic/Pruner/CONSTANT.pm
@@ -1,7 +1,8 @@
 package Bio::PhyloTastic::Pruner::CONSTANT;
 require Exporter;
 @ISA=qw(Exporter);
-@EXPORT_OK=qw(TREEID TAXALIST);
+@EXPORT_OK=qw(TREEID TAXALIST TAXONID);
 sub TREEID   { 'pt.identifier.tree' }
 sub TAXALIST { 'pt.taxaForSubtree'  }
+sub TAXONID  { 'treestoreId' }
 1;
